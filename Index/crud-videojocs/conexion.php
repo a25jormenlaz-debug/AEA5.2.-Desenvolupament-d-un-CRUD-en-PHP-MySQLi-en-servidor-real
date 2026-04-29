92db9aa3198ca7a76a9cabe0c945de47de69d561
a25jormenlaz <a25jormenlaz@inspedralbes.cat>
2026-04-29 10:07:57 +0200

    Update conexion.php

--- Index/crud-videojocs/conexion.php
+++ Index/crud-videojocs/conexion.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $usuario = "a25jormenlaz_AEA5.2";
 $contrasenia = "Mendozalazcano1";
-$base_de_datos = "videojocs";
+$base_de_datos = "videojuegos";
 $mysqli = new mysqli($host, $usuario, $contrasenia, $base_de_datos);
 if ($mysqli->connect_errno) {
     echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
